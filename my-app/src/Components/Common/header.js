4d503c1a910d657f224ba3276e343e1ca6a0b91d
Albert9991 <albert.poghosyan9991@gmail.com>
2020-07-30 15:47:23 +0300

    Getting data from the server using fetch

--- my-app/src/Components/Common/header.js
+++ my-app/src/Components/Common/header.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './header.css';
+import logo from './logo.png';
 
 const Header = () => {
     return(
         <div className="Header">
-
+            <img 
+                src={logo}
+                alt="logo"
+                className="Header-logo"
+             />
         </div>
     )
 };
