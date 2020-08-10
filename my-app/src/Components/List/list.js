ea6a390afa595e591c16cb7775cbfecec9cac3e7
Albert9991 <albert.poghosyan9991@gmail.com>
2020-08-10 16:35:42 +0300

    Created Pagination component

--- my-app/src/Components/List/list.js
+++ my-app/src/Components/List/list.js
@@ -3,6 +3,7 @@ import {API_URL} from '../../config';
 import './table.css'
 import Table from './table'
 import Loading from '../Common/Loading/loading'
+import Pagination from './pagination'
 
 class List extends Component{
     constructor() {
@@ -10,15 +11,21 @@ class List extends Component{
         this.state = {
             currencies: [],
             loading: false,
+            page:1,
+            totalPages:0,
+            perPage:20,
             error:''
         }
+        this.handlePaginationClick = this.handlePaginationClick.bind(this)
     }
 
-    componentDidMount () {
+    fetchCurrencies() {
         this.setState({
             loading: true
          })
-        fetch(`${API_URL}/cryptocurrencies/?page=1&perPage=10`)
+         const{page, perPage}= this.state
+
+        fetch(`${API_URL}/cryptocurrencies/?page=${page}&perPage=${perPage}`)
         .then(resp =>{
             return resp.json().then((data) => {
                 if(resp.ok) {
@@ -28,10 +35,11 @@ class List extends Component{
             })
         })
         .then(data => {
-            const{currencies} = data;
+            const{currencies,totalPages} = data;
             this.setState({
                loading: false,
-               currencies
+               currencies,
+               totalPages
             })
         })
 
@@ -41,10 +49,25 @@ class List extends Component{
                 error:error.errorMessage
             })
         })
+
+    }
+
+    componentDidMount () {
+       this.fetchCurrencies()
+    }
+
+    handlePaginationClick(direction) {
+        let nextPage =this.state.page;
+        nextPage = direction ==='next' ? nextPage +1 : nextPage-1;
+        this.setState({
+            page:nextPage
+        })
+        this.fetchCurrencies()
     }
 
     render() {
-        const { currencies, loading, error } = this.state;
+        console.log(this.state)
+        const { currencies, loading, error,page,totalPages } = this.state;
         if(loading){
             return(
                 <div className="loading-container">
@@ -58,7 +81,16 @@ class List extends Component{
         }
 
         return(
-            <div><Table data={currencies}/></div>
+            <div>
+                <Table 
+                    data={currencies}
+                />
+                <Pagination
+                page={page}
+                totalPages={totalPages}
+                handlePaginationClick = {this.handlePaginationClick}
+                />
+            </div>
         )
         
     }
---
+++ my-app/src/Components/List/pagination.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './pagination.css'
+
+const Pagination = (props) =>{
+    const { handlePaginationClick,page,totalPages } = props;
+    return(
+        <div className="Pagination">
+            <button
+                className='Pagination-button'
+                onClick = {() => handlePaginationClick('prev')}
+                disabled ={page === 1}
+            >
+                 &larr;
+            </button>
+            <span className='Pagination-info'>
+                Page <b>{page}</b> of <b>{totalPages}</b>
+            </span>
+            <button 
+                className='Pagination-button'
+                onClick={() => handlePaginationClick('next')}
+                disabled={page ===totalPages}
+            >
+                &rarr;
+            </button>
+        </div>
+    )
+};
+
+export default Pagination
